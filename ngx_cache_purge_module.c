c7345057ad5429617fc0823e92e3fa8043840cef
Sułowicz Paweł <Pawel.Sulowicz@grupaonet.pl>
2017-02-02 09:39:37 +0100

    Fix compatibility with nginx-1.11.6+

--- ngx_cache_purge_module.c
+++ ngx_cache_purge_module.c
@@ -492,7 +492,11 @@ typedef struct {
     ngx_str_t                      body_source;
 #  endif /* nginx_version < 1007008 */
 
+#  if (nginx_version >= 1011006)
+    ngx_http_complex_value_t      *method;
+#  else
     ngx_str_t                      method;
+#  endif /* nginx_version >= 1011006 */
     ngx_str_t                      location;
     ngx_str_t                      url;
 
